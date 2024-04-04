fc80df10c11937dd39a3f35e7ae2e3437db61e90
AbdulrehmanTs <rohi644@gmail.com>
2024-04-04 10:13:11 +0500

    bgs changes

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -218,20 +218,24 @@ const closeEnquiryPopup = () => {
 // stick header to top
 document.onscroll = () => {
   let header = document.querySelector(".sticky-header");
-  let logo = document.querySelector("#main-logo");
+  let menuLink = document.querySelectorAll(".menu-link");
   let activeLink = document.querySelector(".menu-link.active");
   let headerParent = document.querySelector(".header-parent");
   let body = document.querySelector("body");
   if (window.scrollY > headerParent.offsetTop) {
     header.classList.remove("py-4", 'bg-white');
     header.classList.add("fixed", "top-0", "shadow-lg", "z-10", 'py-2', 'bg-orange-500');
-    activeLink.style.color = 'white'
-    // logo.setAttribute('src', '../assets/images/JustRepairLight.png');
+    [...menuLink].map((item)=> {
+      item.style.setProperty('--priamry', 'white')
+    })
     body.style.paddingTop = header.offsetHeight + "px";
   } else {
     header.classList.add("py-4", 'bg-white');
     header.classList.remove("fixed", "top-0", "shadow-lg", "z-10", 'py-2', 'bg-orange-500');
-    activeLink.style.color = '#f97316'
+    [...menuLink].map((item)=> {
+      item.style.setProperty('--priamry', '#f97316')
+    })
+    activeLink.style.setProperty('--priamry', '#f97316')
     body.style.paddingTop = 0;
   }
 };
